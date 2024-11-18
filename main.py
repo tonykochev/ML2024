da1afe89e6910e03c39ce9c79d8b76c785b52e57
nlange2021@gmail.com <nlange2021@gmail.com>
2024-11-17 19:29:52 -0700

    added links

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 #run algorithm and generate an output that can be published on Kaggle
 #algorithm meshes the weather data with incident reports to output data for severity of incidents
+
+#weather data source: https://www.ncei.noaa.gov/monitoring
+#aviation data source: https://asn.flightsafety.org/database/
