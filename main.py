d4c3351f622a0d6afed9b175ff3582a9407500e9
nlange2021@gmail.com <nlange2021@gmail.com>
2024-11-17 19:43:34 -0700

    link

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #run algorithm and generate an output that can be published on Kaggle
 #algorithm meshes the weather data with incident reports to output data for severity of incidents
 
-#weather data source: https://www.ncei.noaa.gov/monitoring
+#weather data source: https://data.noaa.gov/onestop/
 #aviation data source: https://asn.flightsafety.org/database/
 
 #goal: relate weather conditions to fatalities
@@ -9,4 +9,4 @@
 #aviation relevant data attributes: acc. date, fat., location
 #other attributes?: type - can either manually use this or make type an attribute
 
-#
+#weather relevant data attributes: 
