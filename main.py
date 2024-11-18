c84b0319b484faa75436934c5c6c682f27757f35
nlange2021@gmail.com <nlange2021@gmail.com>
2024-11-17 19:44:56 -0700

    new

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #run algorithm and generate an output that can be published on Kaggle
 #algorithm meshes the weather data with incident reports to output data for severity of incidents
 
-#weather data source: https://data.noaa.gov/onestop/
+#weather data source: https://www.ncei.noaa.gov/cdo-web/
 #aviation data source: https://asn.flightsafety.org/database/
 
 #goal: relate weather conditions to fatalities
